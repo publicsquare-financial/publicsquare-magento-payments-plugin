f186fca05b84ea1e143a07e4fef27de4b5823716
Abraham <abrahambosch@gmail.com>
2024-10-24 15:32:06 -0500

    Feature/sc 67468/investigate bug where a failing createpayment (#15)
    * sc-67468 fixed bugwhere a failing createPayment call clears out the user's cart
    
    * sc-67467 added test for testing checkout with authorize capture
    
    * sc-67467 added test for testing checkout with authorize capture
    
    ---------
    
    Co-authored-by: Abraham Bosch <abraham.bosch@publicsq.com>

---
+++ tests/Acceptance/AcceptanceBase.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class AcceptanceBase
+{
+    const IFRAME_CSS = '#publicsquare-elements-form iframe';
+
+    protected $customerEmail = "";  // this will be dynamicaly produced
+
+
+    protected function _initialize(AcceptanceTester $I): void
+    {
+        $I->amOnPage('/');
+        try {
+            $I->see('Your connection is not private');
+            $this->_getPastBrowserWarning($I);
+        } catch (\Exception $e) {
+            // do nothing if we don't see 'Your connection is not private'
+        }
+    }
+
+    protected function _getPastBrowserWarning(AcceptanceTester $I): void
+    {
+        // TODO: make this conditional to execute only if we see the warning.
+        // get past cert error page
+        $I->click('button[id="details-button"]');
+        $I->waitForElementClickable('#proceed-link');
+        $I->click('#proceed-link');
+    }
+
+    protected function _adminEnableAuthorizeCapture(AcceptanceTester $I): void
+    {
+        $I->amOnPage('/admin');
+
+        // login page
+        $I->fillField('#username', 'admin');
+        $I->fillField('#login', 'AdminPassword123');
+        $I->click('.form-actions .action-login');
+
+        $I->waitForText('Dashboard');
+        $I->click('#menu-magento-backend-stores a');
+        $I->waitForText('Configuration');
+        $I->waitForText('Terms and Conditions');
+        $I->click('.submenu .item-system-config a');
+        $I->waitForText('Country Options');
+        $I->click('#system_config_tabs div.config-nav-block:nth-child(5)');
+        $I->waitForText('Payment Methods');
+        $I->click('Payment Methods');
+
+        // click on button.
+        $I->click('#payment_us_publicsquare_payments-head');
+        $I->waitForText('PublicSquare Secret API key');
+
+        $I->uncheckOption('#payment_us_publicsquare_payments_payment_action_inherit');
+
+        // select sale
+        $I->selectOption('select#payment_us_publicsquare_payments_payment_action', 'sale');
+        $I->click('Save Config');
+        $I->waitForText('You saved the configuration');
+        $I->see('You saved the configuration');
+    }
+
+    protected function _adminEnableAuthorize(AcceptanceTester $I): void
+    {
+        $I->amOnPage('/admin');
+
+        // login page
+        $I->fillField('#username', 'admin');
+        $I->fillField('#login', 'AdminPassword123');
+        $I->click('.form-actions .action-login');
+
+        $I->waitForText('Dashboard');
+        $I->click('#menu-magento-backend-stores a');
+        $I->waitForText('Configuration');
+        $I->waitForText('Terms and Conditions');
+        $I->click('.submenu .item-system-config a');
+        $I->waitForText('Country Options');
+        $I->click('#system_config_tabs div.config-nav-block:nth-child(5)');
+        $I->waitForText('Payment Methods');
+        $I->click('Payment Methods');
+        //$I->selectOption('select#payment_us_recommended_solutions_magento_payments_legacy_apple_pay_payment_action', 'authorize_capture');
+        $I->selectOption('select#payment_us_recommended_solutions_magento_payments_legacy_hosted_fields_payment_action', 'authorize');
+        $I->click('Save Config');
+        $I->waitForText('You saved the configuration');
+        $I->see('You saved the configuration');
+    }
+
+
+    protected function _addProductToCart(AcceptanceTester $I): void
+    {
+        $I->amOnPage('/livingston-all-purpose-tight.html');
+        $I->waitForElement('.swatch-option.text');
+        $I->click('(//div[@class="swatch-option text"])[1]');
+        $I->click('div[data-option-label="Black"]');
+        $I->click('Add to Cart');
+        $I->waitForText('shopping cart');
+    }
+
+
+    protected function _generateUniqueEmail()
+    {
+        $time = time();
+        $this->customerEmail = "BillyBob{$time}@example.com";
+    }
+
+    protected function _goToCheckout(AcceptanceTester $I) {
+        $I->amOnPage('/checkout');
+        $I->waitForElement('#customer-email');
+        $this->_generateUniqueEmail();
+        $I->fillField('#customer-email', $this->customerEmail);
+        $I->fillField('firstname', 'Billy');
+        $I->fillField('lastname', 'Bob');
+        $I->fillField('street[0]', '123 Main St');
+        $I->selectOption('select[name="country_id"]', 'US');
+        $I->selectOption('select[name="region_id"]', '15');
+        $I->fillField('city', 'Newark');
+        $I->fillField('postcode', '19711');
+        $I->fillField('telephone', '1234567890');
+        $firstRadio = '.table-checkout-shipping-method tbody tr:nth-child(1) input[type="radio"]';
+        $I->waitForElementClickable($firstRadio);
+        $I->click($firstRadio);
+        $I->click('Next');
+    }
+
+    protected function _makeSurePaymentMethodIsVisible(AcceptanceTester $I)
+    {
+        $publicsquarePayments = '#publicsquare_payments';
+        $I->waitForElementClickable($publicsquarePayments);
+        $I->click($publicsquarePayments);
+        $I->see('Credit/Debit Card Number');
+        $I->waitForElementVisible($this::IFRAME_CSS);
+        $x = $I->grabAttributeFrom($this::IFRAME_CSS, 'id');
+        $I->switchToIframe('//*[@id="'.$x.'"]');
+        $I->waitForElementVisible('//*[@id="cardNumber"]');
+        $I->waitForElementVisible('//*[@id="expirationDate"]');
+        $I->waitForElementVisible('//*[@id="cvc"]');
+        $I->switchToIframe();
+    }
+
+    protected function _checkoutWithCard(AcceptanceTester $I)
+    {
+        $this->_makeSurePaymentMethodIsVisible($I);
+        $I->waitForElementVisible($this::IFRAME_CSS);
+        $x = $I->grabAttributeFrom($this::IFRAME_CSS, 'id');
+        $I->switchToIframe('//*[@id="'.$x.'"]');
+        $I->fillField('//*[@id="cardNumber"]', '4242424242424242');
+        $I->fillField('//*[@id="expirationDate"]', '12/29');
+        $I->fillField('//*[@id="cvc"]', '123');
+        $I->switchToIframe();
+        $submitButton = '.payment-method._active button[type="submit"]';
+        $I->waitForElementClickable($submitButton);
+        $I->click($submitButton);
+        $I->waitForText('Thank you for your purchase!');
+    }
+}
