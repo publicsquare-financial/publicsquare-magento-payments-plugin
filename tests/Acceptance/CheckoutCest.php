0b1c0638f04a1342e174614ee20950f67e54ada8
Ryan Frahm <ryan.frahm@publicsq.com>
2024-10-28 16:29:36 -0500

    feat: improvements to error handling and logging (#21)
    * feat: improvements to error handling and logging
    
    * release: bump version

--- tests/Acceptance/CheckoutCest.php
+++ tests/Acceptance/CheckoutCest.php
@@ -6,6 +6,9 @@ use Tests\Support\AcceptanceTester;
 
 class CheckoutCest extends AcceptanceBase
 {
+    const GENERIC_DECLINE_MESSAGE = 'The payment could not be processed. Reason: ';
+    const GENERIC_FRAUDULENT_MESSAGE = 'The payment could not be completed. Please verify your details and try again.';
+
     public function checkoutWorks(AcceptanceTester $I)
     {
         $this->_initialize($I);
@@ -43,5 +46,37 @@ class CheckoutCest extends AcceptanceBase
         // verify order was not created.
         $I->dontSeeInDatabase('sales_order', ['customer_email' => $this->customerEmail]);
 
+    }
+
+    public function makeSureDeclinedChargesHaveCorrectErrorMessages(AcceptanceTester $I)
+    {
+        $this->_initialize($I);
+
+        // add product.
+        $this->_addProductToCart($I);
+
+        // do checkout
+        $this->_goToCheckout($I);
+
+        // Reason: Declined
+        $this->_checkoutWithCard($I, '4000000000000002', $this::GENERIC_DECLINE_MESSAGE.'Decline');
+        $I->reloadPage();
+        // Reason: Insufficient Funds
+        $this->_checkoutWithCard($I, '4000000000009995', $this::GENERIC_DECLINE_MESSAGE.'Insufficient Funds');
+        $I->reloadPage();
+        // Reason: Lost/Stolen
+        $this->_checkoutWithCard($I, '4000000000009987', $this::GENERIC_DECLINE_MESSAGE.'Lost/Stolen');
+        $I->reloadPage();
+        // Reason: Rejected fraud decision
+        $this->_checkoutWithCard($I, '4100000000000019', $this::GENERIC_FRAUDULENT_MESSAGE);
+        $I->reloadPage();
+        // Reason: CVC check fails
+        $this->_checkoutWithCard($I, '4000000000000101', $this::GENERIC_FRAUDULENT_MESSAGE);
+        $I->reloadPage();
+        // Reason: Address check fails
+        $this->_checkoutWithCard($I, '4000000000000010', $this::GENERIC_FRAUDULENT_MESSAGE);
+
+        // verify order was not created.
+        $I->dontSeeInDatabase('sales_order', ['customer_email' => $this->customerEmail]);
     }
 }
