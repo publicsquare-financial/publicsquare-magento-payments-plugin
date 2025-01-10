3a5306e173d70fee0ec6474799182e0f333e9714
Abraham Bosch <abraham.bosch@publicsq.com>
2025-01-10 00:05:17 -0600

    added tests for the order confirmation email and the invoice email

--- tests/Acceptance/CustomerPaymentMethodInfoCest.php
+++ tests/Acceptance/CustomerPaymentMethodInfoCest.php
@@ -19,6 +19,9 @@ class CustomerPaymentMethodInfoCest extends AcceptanceBase
 
         $this->_checkoutWithCard($I);
 
+        // get the order number
+        $orderNumber = $I->executeJS('return jQuery(".order-number").text(); ');
+
         // check order payment method info
         $this->_customerGoToAnOrder($I, false);
 
@@ -29,6 +32,38 @@ class CustomerPaymentMethodInfoCest extends AcceptanceBase
         $I->see('xxxx-4242');
 
         // verify that customers don't see these payment method details.
+        $I->dontSee('Payment ID:');
+        $I->dontSee('AVS Response:');
+        $I->dontSee('CVV Response');
+        $I->dontSee('Fraud Decision');
+
+        // verify emails
+        $I->amOnUrl('http://localhost:1080/');
+        $I->see("MailCatcher");
+        $I->waitForElementVisible("#messages");
+
+        // make sure both emails are present.
+        $I->see("Invoice for your Main Website Store order");
+        $I->see("Your Main Website Store order confirmation");
+
+        // verify invoice email
+        $I->click('//td[contains(text(), "Invoice for your Main Website Store order")]');
+        $I->switchToIframe("#message .body");
+        $I->waitForText("Credit/Debit Card");
+        $I->see("Order #$orderNumber");
+        $I->see('Credit/Debit Card');
+        $I->dontSee('Payment ID:');
+        $I->dontSee('AVS Response:');
+        $I->dontSee('CVV Response');
+        $I->dontSee('CVV Response');
+        $I->dontSee('Fraud Decision');
+
+        // verify order confirmation email
+        $I->switchToIframe();   // Switch back to the main page (outside the iframe)
+        $I->click('//td[contains(text(), "Your Main Website Store order confirmation")]');
+        $I->switchToIframe("#message .body");
+        $I->see("Order #$orderNumber");
+        $I->see('Credit/Debit Card');
         $I->dontSee('Payment ID:');
         $I->dontSee('AVS Response:');
         $I->dontSee('CVV Response');
