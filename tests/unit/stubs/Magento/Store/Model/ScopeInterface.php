69720499260f299e305d5f561bfa3fadc4c42907
James Jones <james.jones@credova.com>
2025-10-24 13:32:19 -0400

    Removing bootstrap eval in place of stubs

---
+++ tests/unit/stubs/Magento/Store/Model/ScopeInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Magento\Store\Model;
+
+interface ScopeInterface
+{
+    const SCOPE_STORE = 'store';
+    const SCOPE_WEBSITE = 'website';
+    const SCOPE_DEFAULT = 'default';
+}
+
+
