77390a62e802db3c5ae3724ec8f8157b1a57d861
James Jones <james.jones@credova.com>
2025-11-04 11:03:47 -0500

    Cleaning up comments

--- PublicSquare/Payments/Block/Frontend/Form.php
+++ PublicSquare/Payments/Block/Frontend/Form.php
@@ -1,21 +1,8 @@
 <?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-declare(strict_types=1);
-
 namespace PublicSquare\Payments\Block\Frontend;
 
 use PublicSquare\Payments\Block\Form as BaseForm;
 
-/**
- * Frontend payment form block for PublicSquare Payments.
- *
- * This extends the base Form block to reuse all logic (card types filtering,
- * CVV config, vault enablement, etc.) while allowing a distinct class to be
- * referenced from frontend DI configuration.
- */
 class Form extends BaseForm
 {
     /**
