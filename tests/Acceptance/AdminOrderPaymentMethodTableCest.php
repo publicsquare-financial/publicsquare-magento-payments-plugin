63bbce69154f49981bdd127a42c9b8e17f2d23ad
ben.tilford <214548783+btilfordpubsq@users.noreply.github.com>
2025-12-04 14:44:24 -0700

    chore: Admin order test is using normal checkout flow
    **Tickets:**
    * [sc-81470](https://app.shortcut.com/publicsq/story/81470)

--- tests/Acceptance/AdminOrderPaymentMethodTableCest.php
+++ tests/Acceptance/AdminOrderPaymentMethodTableCest.php
@@ -5,17 +5,19 @@ namespace Tests\Acceptance;
 use Tests\Pages\AdminOrderDetailPage;
 use Tests\Support\AcceptanceTester;
 
+
 class AdminOrderPaymentMethodTableCest extends AdminOrderDetailPage
 {
     public function paymentInformationTableIsVisible(AcceptanceTester $I, )
     {
-        $this->_doSuccessfulCheckout($I, '4242424242424242', 'Thank you for your purchase!', false, false, '#publicsquare-elements-form iframe');
+        // This goes to normal checkout...
+        $this->_doSuccessfulCheckout($I);
         $this->_goToOrderDetail($I);
         $this->_paymentInformationTableIsVisible($I);
     }
     public function paymentDetailsLinkHasCorrectUrl(AcceptanceTester $I)
     {
-        $this->_doSuccessfulCheckout($I, '4242424242424242', 'Thank you for your purchase!', false, false, '#publicsquare-elements-form iframe');
+        $this->_doSuccessfulCheckout($I);
         $this->_goToOrderDetail($I);
         $this->_paymentDetailsLinkHasCorrectUrl($I);
     }
