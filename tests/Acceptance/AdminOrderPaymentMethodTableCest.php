274db112fe99f51233974a5cafd9e680e7024ade
ben.tilford <214548783+btilfordpubsq@users.noreply.github.com>
2025-12-04 14:07:19 -0700

    fix(AcceptanceTest): Admin order has 2 tests
    **Tickets:**
    * [sc-81470](https://app.shortcut.com/publicsq/story/81470)

--- tests/Acceptance/AdminOrderPaymentMethodTableCest.php
+++ tests/Acceptance/AdminOrderPaymentMethodTableCest.php
@@ -9,7 +9,7 @@ class AdminOrderPaymentMethodTableCest extends AdminOrderDetailPage
 {
     public function paymentInformationTableIsVisible(AcceptanceTester $I, )
     {
-        $this->_doSuccessfulCheckout($I);
+        $this->_doSuccessfulCheckout($I, '4242424242424242', 'Thank you for your purchase!', false, false, '#publicsquare-elements-form iframe');
         $this->_goToOrderDetail($I);
         $this->_paymentInformationTableIsVisible($I);
     }
