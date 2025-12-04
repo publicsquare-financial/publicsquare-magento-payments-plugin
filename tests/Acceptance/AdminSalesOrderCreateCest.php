9838e01a4d02323e05b7e2c80476f8779f5c8f92
ben.tilford <214548783+btilfordpubsq@users.noreply.github.com>
2025-12-04 16:21:22 -0700

    fix: Fix for incorrect iframe selector
    **Tickets:**
    * [sc-81470](https://app.shortcut.com/publicsq/story/81470)

--- tests/Acceptance/AdminSalesOrderCreateCest.php
+++ tests/Acceptance/AdminSalesOrderCreateCest.php
@@ -29,7 +29,7 @@ class AdminSalesOrderCreateCest extends AdminSalesOrderCreate
             $this->_submitOrder($I, 'The payment could not be processed. Reason: Insufficient Funds');
             echo "First payment method successfully failed\n";
             // Then, succeed the payment
-            $this->_fillCardForm($I, '4242424242424242', '12/29', '123', '#publicsquare-elements-form');
+            $this->_fillCardForm($I, '4242424242424242', '12/29', '123', '#publicsquare-elements-form', '#publicsquare-elements-form');
             $this->_submitOrder($I);
             echo "Second payment method successfully succeeded\n";
         }catch (\Exception $exception){
