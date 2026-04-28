477e22316a049fd18e04da5397cf75458d563520
ben.tilford <214548783+btilfordpubsq@users.noreply.github.com>
2026-04-28 11:38:10 -0600

    fix(psq-elements): Use a versioned url for elements
    # **Tickets:**
    # * [sc-XXXXX](https://app.shortcut.com/publicsq/story/XXXXX)
    #

--- PublicSquare/Payments/view/frontend/requirejs-config.js
+++ PublicSquare/Payments/view/frontend/requirejs-config.js
@@ -1,7 +1,7 @@
 var config = {
     map: {
         '*': {
-            'publicsquarejs': 'https://js.publicsquare.com',
+            'publicsquarejs': 'https://js.publicsquare.com/v1.9.11/',
             'publicsquare_payments': 'PublicSquare_Payments/js/publicsquare_payments',
         },
     },
